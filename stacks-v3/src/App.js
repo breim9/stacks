d1a1eee51a18a15b7b41ce74230c558af2839e25
Ben Reimer <benreimer9@gmail.com>
2019-05-02 09:45:05 -0400

    test PR setup

--- stacks-v3/src/App.js
+++ stacks-v3/src/App.js
@@ -506,23 +506,7 @@ class App extends Component {
     }
     return today;
   };
-  dayController = () => {
-    //get the day
-    let fullDate = new Date();
-    let thisDay = fullDate.getDate().toString();
-    let thisMonth = fullDate.getMonth().toString();
-    let thisYear = fullDate.getFullYear().toString();
-
-    //build dates
-    let currentDate = thisDay + "/" + thisMonth + "/" + thisYear;
-    let visDate = this.visualDate(thisDay, thisMonth);
-
-    //check if is new day
-    if (this.isNewDay(currentDate, visDate)) {
-      this.newDayUpdateStreakCounter();
-      this.resetForNewDay();
-    }
-  };
+  dayController = () => {};
   isNewDay = (currentDate, visDate) => {
     let date = { ...this.state.date };
     let lastLoggedDate = date.lastLoggedDate;
